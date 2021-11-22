5709b7c5e2ae4d22e031179cbe805e64fe438c14
xas009 <akane.morita11@gmail.com>
2021-11-22 08:09:33 +0000

    added validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,22 @@
 class MessagesController < ApplicationController
     def create
-        message = Message.new(message_params)
-        message.chat_room_id = params[:chat_room_id]
-        message.user_id = current_user.id
-        message.save!
-        redirect_to chat_room_path(message.chat_room_id)
+        @message = Message.new(message_params)
+        @message.chat_room_id = params[:chat_room_id]
+        @message.user_id = current_user.id
+        if @message.save
+            redirect_to chat_room_path(@message.chat_room_id)
+        else
+            @chat_room = ChatRoom.find(params[:chat_room_id])
+            @users = @chat_room.users
+              @users.each do |user|
+                if user != current_user
+                  @user = user
+                end
+                break if @user
+              end
+            @messages = @chat_room.messages
+            render template: "chat_rooms/show"
+        end
     end
 
 private
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chat_room
+  
+  validates :text, presence: true, length: { maximum: 140 }
 end
